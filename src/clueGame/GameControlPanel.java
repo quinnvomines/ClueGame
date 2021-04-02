abc64b45668d7557f0597225f8444c7ab19bf709
Quinn <quinnvo3004724@gmail.com>
2021-04-02 16:38:20 -0600

    Make JTextFields non-editable

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -26,7 +26,7 @@ public class GameControlPanel extends JPanel {
 		//Main game control panel
 		gamePanel = new JPanel();
 		gamePanel.setLayout(new GridLayout(2, 0));
-		add(gamePanel, BorderLayout.CENTER);
+		add(gamePanel, BorderLayout.SOUTH);
 
 		makeTopPanel(); //Top panel
 		makeBottomPanel(); //Bottom panel
@@ -54,12 +54,14 @@ public class GameControlPanel extends JPanel {
 		//Turn
 		JLabel turnMessage = new JLabel("Whose turn?");
 		turn = new JTextField(20);
+		turn.setEditable(false);
 		panel_1.add(turnMessage);
 		panel_1.add(turn);
 
 		//Roll
 		JLabel rollMessage = new JLabel("Roll:");
 		roll = new JTextField(20);
+		roll.setEditable(false);
 		panel_2.add(rollMessage);
 		panel_2.add(roll);
 	}
@@ -79,7 +81,9 @@ public class GameControlPanel extends JPanel {
 
 		//Allocate space for text fields
 		guess = new JTextField(20);
+		guess.setEditable(false);
 		guessResult = new JTextField(20);
+		guessResult.setEditable(false);
 
 		//Add panels to bottom panel
 		panel.add(panel_1);
@@ -124,16 +128,16 @@ public class GameControlPanel extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
 
-		
-		
+
+
 		// test filling in the data
 		Board board = Board.getInstance();
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");		
 		board.initialize();
 		panel.setTurn(new ComputerPlayer( "Col. Mustard", "orange", 0, 0, board), 5);
-		panel.setGuess( "I have no guess!");
-		panel.setGuessResult( "So you have nothing?");
-		
+		panel.setGuess( "I have no guess! ");
+		panel.setGuessResult( "So you have nothing? ");
+
 
 	}
 
