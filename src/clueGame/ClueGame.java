0ed26b33ce4f560a24bf461c2b3d6dc008c7b364
Quinn <quinnvo3004724@gmail.com>
2021-04-07 20:50:43 -0600

    refactored to add comments and refactored to fix layering of doors

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -7,24 +7,30 @@ import javax.swing.JFrame;
 public class ClueGame extends JFrame{
 	
 	public ClueGame(Board b) {
-		setSize(700, 650);
-		setTitle("Clue Game");
+		setSize(700, 650); //Size
+		setTitle("Clue Game"); //Title
 		
+		//Add the Board panel to the center of the window
 		add(b, BorderLayout.CENTER);
 		
+		//Game control panel
 		GameControlPanel gamePanel = new GameControlPanel();
 		add(gamePanel, BorderLayout.SOUTH);
 		
+		//Known cards panel
 		KnownCardsPanel knownPanel = new KnownCardsPanel();
 		add(knownPanel, BorderLayout.EAST);
 		
 	}
 	
 	public static void main(String [] args) {
+		//Set up board
 		Board board = Board.getInstance();
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
 		board.initialize();
-		board.repaint();
+		
+		board.repaint(); //PAINT
+		
 		ClueGame cg = new ClueGame(board);
 		cg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		cg.setVisible(true);
